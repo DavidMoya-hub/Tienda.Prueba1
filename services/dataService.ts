6c9dd29c3cfd538604b69b8508b7df7a91e8d20e
DavidMoya-hub <84820848+DavidMoya-hub@users.noreply.github.com>
2026-03-21 20:46:43 -0600

    feat: Update Google Apps Script API endpoint
    Replaced the existing Google Apps Script API endpoint URL with a new one. This is likely to point to an updated or re-deployed version of the backend script.

--- services/dataService.ts
+++ services/dataService.ts
@@ -1,6 +1,6 @@
 import { Product, InputTransaction, OutputTransaction, DailyClosing, PriceHistory, PurchaseNote, Envelope, EnvelopeWithdrawal } from "../types";
 
-const API_URL = "https://script.google.com/macros/s/AKfycbwtbTH2N8YvparbQFLh3sCsJ5EEaq9XcBx6IkdlOki3Ca0cy3P6vaDgft3glDgBNuwRdw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxkztsqc3xH0r2dDanKRKohYAZQG00Dwh4plfrf-dYiiC1cDkUAP45SlJ6DKP_5JxyWLQ/exec";
 
 declare var google: any;
 
